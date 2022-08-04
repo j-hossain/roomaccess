e7581574ce3da65e1a364fcc3c09ff08e838606a
j-hossain <jahin.hsn101@gmail.com>
2022-08-04 06:40:06 +0600

    New check method is working

--- app/src/main/java/com/example/roomaccess/Home.java
+++ app/src/main/java/com/example/roomaccess/Home.java
@@ -1,14 +1,21 @@
 package com.example.roomaccess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Home extends AppCompatActivity {
     Button btn_qrscan, btn_logout, btn_my_rooms;
@@ -19,6 +26,9 @@ public class Home extends AppCompatActivity {
         btn_qrscan = (Button) findViewById(R.id.btn_qrscan);
         btn_logout = (Button) findViewById(R.id.btn_logout);
         btn_my_rooms = (Button) findViewById(R.id.btn_my_rooms);
+
+
+        getUserName();
 
         btn_qrscan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +52,28 @@ public class Home extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(Home.this,MyRooms.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    public void getUserName(){
+        FirebaseAuth fr_auth = FirebaseAuth.getInstance();
+        FirebaseUser user = fr_auth.getCurrentUser();
+
+        DatabaseReference db_ref = FirebaseDatabase.getInstance().getReference();
+        db_ref.child("Users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    model_user us = snapshot.getValue(model_user.class);
+                    us.setUserID(user.getUid());
+                    TextView tv_user_name = findViewById(R.id.tv_user_name);
+                    tv_user_name.setText(us.getName().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
--- app/src/main/java/com/example/roomaccess/userRoomAccess.java
+++ app/src/main/java/com/example/roomaccess/userRoomAccess.java
@@ -9,14 +9,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class userRoomAccess extends AppCompatActivity {
     DatabaseReference db_ref;
@@ -31,32 +34,51 @@ public class userRoomAccess extends AppCompatActivity {
         String userID = user.getUid().toString();
         roomID = getIntent().getExtras().getString("roomID");
         TextView tv_access = findViewById(R.id.tv_access_status);
-        db_ref.child("Access").child(roomID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        db_ref.child("Access").child(roomID).child(userID).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(!task.isSuccessful()){
-                    Log.e("firebase","Error Getting Access Data",task.getException());
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    Toast.makeText(getApplicationContext(),"Access Granted",Toast.LENGTH_LONG).show();
+                    db_ref.child("Rooms").child(roomID).child("Command").setValue("unlock");
+                    Toast.makeText(getApplicationContext(),"Lock Opening",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(userRoomAccess.this,Home.class));
                 }
                 else{
-
-                    TextView testing = findViewById(R.id.tv_testing);
-                    String userstring="-1";
-                    for (DataSnapshot user:task.getResult().getChildren()){
-                        if(userID.matches(user.getKey().toString())){
-                            userstring = user.getKey().toString();
-                            break;
-                        }
-                    }
-                    if(userstring.matches("-1")){
-                        tv_access.setText("You do not have access to this room");
-                    }
-                    else{
-                        tv_access.setText("You have access to this room");
-                        db_ref.child("Rooms").child(roomID).child("Command").setValue("unlock");
-                    }
+                    tv_access.setText("No Access");
                 }
             }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
         });
+
+//        db_ref.child("Access").child(roomID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DataSnapshot> task) {
+//                if(!task.isSuccessful()){
+//                    Log.e("firebase","Error Getting Access Data",task.getException());
+//                }
+//                else{
+//
+//                    TextView testing = findViewById(R.id.tv_testing);
+//                    String userstring="-1";
+//                    for (DataSnapshot user:task.getResult().getChildren()){
+//                        if(userID.matches(user.getKey().toString())){
+//                            userstring = user.getKey().toString();
+//                            break;
+//                        }
+//                    }
+//                    if(userstring.matches("-1")){
+//
+//                    }
+//                    else{
+//
+//                    }
+//                }
+//            }
+//        });
         Button btn_home = findViewById(R.id.btn_home);
         btn_home.setOnClickListener(new View.OnClickListener() {
             @Override
